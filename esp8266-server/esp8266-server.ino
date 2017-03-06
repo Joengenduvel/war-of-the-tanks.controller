96cce8b324540a2c707fb746444096aa2b11d45b
Robby Decosemaeker <robby.decosemaeker@telenet.be>
2017-03-06 21:57:40 +0100

    Added motor controls

--- esp8266-server/esp8266-server.ino
+++ esp8266-server/esp8266-server.ino
@@ -12,12 +12,27 @@ WiFiServer server(80);
 
 
 #define USE_SERIAL Serial
+#define PIN_LEFT_DIRECTION1  5
+#define PIN_LEFT_DIRECTION2  4
+#define PIN_LEFT_ENABLE 2
+
+
+#define PIN_RIGHT_DIRECTION1  12
+#define PIN_RIGHT_DIRECTION2  13
+#define PIN_RIGHT_ENABLE  14
 
 void setup() {
-  // USE_SERIAL.begin(921600);
+  pinMode(PIN_LEFT_DIRECTION1, OUTPUT);
+  pinMode(PIN_LEFT_DIRECTION2, OUTPUT);
+  pinMode(PIN_LEFT_ENABLE, OUTPUT);
+
+
+  pinMode(PIN_RIGHT_DIRECTION1, OUTPUT);
+  pinMode(PIN_RIGHT_DIRECTION2, OUTPUT);
+  pinMode(PIN_RIGHT_ENABLE, OUTPUT);
+
   USE_SERIAL.begin(115200);
 
-  //Serial.setDebugOutput(true);
   USE_SERIAL.setDebugOutput(true);
 
   USE_SERIAL.println();
@@ -56,7 +71,7 @@ void loop() {
   }
 }
 
-void sendWebPage(WiFiClient client){
+void sendWebPage(WiFiClient client) {
   USE_SERIAL.print("sending page to: ");
   USE_SERIAL.println(client.localIP());
 
@@ -65,7 +80,7 @@ void sendWebPage(WiFiClient client){
   client.println(""); // do not forget this one
   client.print("<!DOCTYPE html><html><script type=\"text/javascript\">document.addEventListener('touchstart', process_touchstart, false);var ws=new WebSocket(\"ws://");
   client.print(WiFi.localIP());
-  client.println(":81\");function process_touchstart(ev){switch (ev.touches.length){case 1: enable_movement(ev); break; case 2: handle_two_touches(ev); break; case 3: handle_three_touches(ev); break; default: gesture_not_supported(ev); break;}}function enable_movement(ev){document.addEventListener('touchmove', send_positions, false);document.addEventListener('touchend', ws.send(\"STOP\"), false);}function send_positions(ev){var resultPane=document.getElementById(\"result\"); var touchobj=ev.changedTouches[0];var halfWidth=window.innerWidth/2;var halfHeight=window.innerHeight/2;var travelX=parseInt(((touchobj.clientX - halfWidth)/halfWidth)*100);var travelY=parseInt(((halfHeight - touchobj.clientY)/halfHeight)*100);resultPane.textContent=travelX +' - ' + travelY;ws.send(travelX + '/' + travelY);}</script><body><div style=\"position:absolute;top:50%; left:50%;transform: translateY(-50%);transform: translateX(-50%);\"><p>Touch Me!</p><p id=\"result\"></p></div></body></html>");
+  client.println(":81\");function process_touchstart(ev){switch (ev.touches.length){case 1: enable_movement(ev); break; case 2: handle_two_touches(ev); break; case 3: handle_three_touches(ev); break; default: gesture_not_supported(ev); break;}}function enable_movement(ev){document.addEventListener('touchmove', send_positions, false);document.addEventListener('touchend', ws.send(\"0/0\"), false);}function send_positions(ev){var resultPane=document.getElementById(\"result\"); var touchobj=ev.changedTouches[0];var halfWidth=window.innerWidth/2;var halfHeight=window.innerHeight/2;var travelX=parseInt(((touchobj.clientX - halfWidth)/halfWidth)*100);var travelY=parseInt(((halfHeight - touchobj.clientY)/halfHeight)*100);resultPane.textContent=travelX +' - ' + travelY;ws.send(travelX + '/' + travelY);}</script><body><div style=\"position:absolute;top:50%; left:50%;transform: translateY(-50%);transform: translateX(-50%);\"><p>Touch Me!</p><p id=\"result\"></p></div></body></html>");
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
@@ -83,14 +98,76 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
         webSocket.sendTXT(num, "Connected");
       }
       break;
-    case WStype_TEXT:
-      USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
-
-      // send message to client
-      // webSocket.sendTXT(num, "message here");
-
-      // send data to all connected clients
-      // webSocket.broadcastTXT("message here");
+    case WStype_TEXT: {
+        USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+        String x = String(strtok((char *)payload, "/"));
+        String y = String(strtok(NULL, "/"));
+        //determine direction
+        bool left = x.startsWith("-");
+        if (left) {
+          x = String(x.substring(1));
+        }
+        bool reverse = y.startsWith("-");
+        if (reverse) {
+          y = String(y.substring(1));
+        }
+        int absX = limit(x.toInt());
+        int absY = limit(y.toInt());
+        if (left) {
+          absX *= -1;
+        }
+        if (reverse) {
+          absY *= -1;
+        }
+        //initial speeds
+        int leftInitialSpeed = absY - absX;
+        int rightInitialSpeed = absY + absX;
+        //scale proportional to 100%
+        float scaleFactor = 1;
+        if (!(leftInitialSpeed == 0 || rightInitialSpeed == 0)) {
+          int absLeft = abs(leftInitialSpeed);
+          int absRight = abs(rightInitialSpeed);
+          if (absLeft > 100 || absRight > 100) {
+            if (absLeft > absRight) {
+              scaleFactor = 100.0 / absLeft;
+            } else {
+              scaleFactor = 100.0 / absRight;
+            }
+          }
+        }
+        USE_SERIAL.println("initial speed");
+        USE_SERIAL.print(leftInitialSpeed);
+        USE_SERIAL.print(", ");
+        USE_SERIAL.println(rightInitialSpeed);
+        int leftSpeed = calculateSpeed(abs(leftInitialSpeed * scaleFactor));
+        int rightSpeed = calculateSpeed(abs(rightInitialSpeed * scaleFactor));
+        USE_SERIAL.printf("scaled speed by: %d\r\n", scaleFactor);
+        USE_SERIAL.print(leftSpeed);
+        USE_SERIAL.print(", ");
+        USE_SERIAL.println(rightSpeed);
+        //set enable pins to match motor direction
+        if (leftInitialSpeed > 0) {
+          USE_SERIAL.println("left forward");
+          digitalWrite(PIN_LEFT_DIRECTION1, HIGH);
+          digitalWrite(PIN_LEFT_DIRECTION2, LOW);
+        } else {
+          USE_SERIAL.println("left backward");
+          digitalWrite(PIN_LEFT_DIRECTION1, LOW);
+          digitalWrite(PIN_LEFT_DIRECTION2, HIGH);
+        }
+        if (rightInitialSpeed > 0) {
+          USE_SERIAL.println("right forward");
+          digitalWrite(PIN_RIGHT_DIRECTION1, HIGH);
+          digitalWrite(PIN_RIGHT_DIRECTION2, LOW);
+        } else {
+          USE_SERIAL.println("right backward");
+          digitalWrite(PIN_RIGHT_DIRECTION1, LOW);
+          digitalWrite(PIN_RIGHT_DIRECTION2, HIGH);
+        }
+        //set PWM for both motors
+        analogWrite(PIN_LEFT_ENABLE, leftSpeed);
+        analogWrite(PIN_RIGHT_ENABLE, rightSpeed);
+      }
       break;
     case WStype_BIN:
       USE_SERIAL.printf("[%u] get binary lenght: %u\n", num, lenght);
@@ -102,3 +179,16 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
   }
 
 }
+
+
+int calculateSpeed (int percentage) {
+  return map(percentage, 0, 100, 0, 1023);
+}
+
+int limit(int value) {
+  if (value > 100) {
+    return 100;
+  }
+  return value;
+}
+
